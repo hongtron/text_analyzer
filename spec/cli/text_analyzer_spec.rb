562bc31ac3a8d4981e9c398709ba255d6b13a486
ali hong <ali.hong@getbraintree.com>
2020-09-15 15:51:31 +0000

    add bin script and cli specs

---
+++ spec/cli/text_analyzer_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe "bin/text_analyzer" do
+  it "accepts arguments as a list of one or more file paths"
+  it "accepts input on stdin"
+  it "displays help text if no input is provided"
+end
