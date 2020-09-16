482d5929a6fab24087eb80eff63b4e35332f80f4
ali hong <ali.hong@getbraintree.com>
2020-09-16 19:32:34 +0000

    update bin spec

--- spec/bin/text_analyzer_spec.rb
+++ spec/bin/text_analyzer_spec.rb
@@ -1,12 +1,13 @@
 RSpec.describe "bin/text_analyzer" do
   it "accepts arguments as a list of one or more file paths" do
-    out = %x[DEBUG=true bin/text_analyzer file1.txt file2.txt]
-    expect(out).to match("[DEBUG] Analyzing file1.txt")
-    expect(out).to match("[DEBUG] Analyzing file2.txt")
+    project_root = File.expand_path("../../..", __FILE__)
+    out = %x[cd #{project_root} && DEBUG=true bin/text_analyzer README.md SPECIFICATION.md]
+    expect(out).to match("Analyzing README.md")
+    expect(out).to match("Analyzing SPECIFICATION.md")
   end
 
   it "accepts input on stdin" do
-    expect(%x[echo "yes hi hello" | DEBUG=true bin/text_analyzer]).to match("[DEBUG] Analyzing STDIN")
+    expect(%x[echo "yes hi hello" | DEBUG=true bin/text_analyzer]).to match("Analyzing STDIN")
   end
 
   it "displays help text if passed args that are not filenames" do
