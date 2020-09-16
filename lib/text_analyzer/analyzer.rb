3f37d160d87b97920fa62a95bf7e24389d37ef0a
ali hong <ali.hong@getbraintree.com>
2020-09-16 18:46:17 +0000

    implement analysis

--- lib/text_analyzer/analyzer.rb
+++ lib/text_analyzer/analyzer.rb
@@ -1,17 +1,55 @@
 module TextAnalyzer
   class Analyzer
-    attr_accessor :input
+    SEQUENCE_SIZE = 3
+    RANK_CUTOFF = 100
 
     def initialize(input)
       @input = input
     end
 
     def run
-      if input == STDIN
-        analyze(input)
+      results = if @input == STDIN
+        analyze(@input)
       else
-        # thread it
+        individual_results = @input.map { |path| File.new(path) }
+          .map { |file| Thread.new { Thread.current[:result] = analyze(file) } }
+          .each(&:join)
+          .map { |t| t[:result] }
+        combined_results = individual_results.reduce(Hash.new(0)) do |acc, result|
+          acc.merge(result) { |seq, total_count, current_count| total_count + current_count }
+        end
       end
+
+      get_most_common(results)
+    end
+
+    def analyze(input)
+      # TODO: test that inputs of less than 3 words return empty results
+      TextAnalyzer::LOGGER.debug("Analyzing #{input == STDIN ? "STDIN" : input.path}")
+      result = Hash.new(0)
+      lookback_tokens = []
+      input.each_line do |line|
+        normalize!(line)
+        tokens = line.split(' ')
+        sequences(tokens).each { |seq| result[seq] += 1 }
+      end
+
+      result
+    end
+
+    def normalize!(text)
+      text.downcase!
+      text.gsub!(/\s+/, ' ')
+      text.gsub!(/[^a-zA-Z0-9_ ]/,'')
+      text
+    end
+
+    def sequences(tokens)
+      tokens.each_cons(SEQUENCE_SIZE)
+    end
+
+    def get_most_common(result)
+      result.sort_by(&:last).reverse!.take(RANK_CUTOFF).compact
     end
   end
 end
--- spec/bin/text_analyzer_spec.rb
+++ spec/bin/text_analyzer_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe "bin/text_analyzer" do
   it "accepts arguments as a list of one or more file paths" do
     out = %x[DEBUG=true bin/text_analyzer file1.txt file2.txt]
-    expect(out).to match("[DEBUG] Processing file1.txt")
-    expect(out).to match("[DEBUG] Processing file2.txt")
+    expect(out).to match("[DEBUG] Analyzing file1.txt")
+    expect(out).to match("[DEBUG] Analyzing file2.txt")
   end
 
   it "accepts input on stdin" do
-    expect(%x[echo "yes hi hello" | DEBUG=true bin/text_analyzer]).to match("[DEBUG] Processing from STDIN")
+    expect(%x[echo "yes hi hello" | DEBUG=true bin/text_analyzer]).to match("[DEBUG] Analyzing STDIN")
   end
 
   it "displays help text if passed args that are not filenames" do
